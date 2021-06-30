0317172808c1a550ea63af9fe8be58dfaec23d2d
JohnBensen1000 <jbensen@purdue.edu>
2021-06-30 17:37:42 -0400

    completely redid intro page, now has arrow pointing down

--- main.js
+++ main.js
@@ -1,15 +1,35 @@
 const navButtons =
-    ["nav-about-me", "nav-entropy", "nav-rocket-guidance", "nav-this-website"];
+    ["nav-entropy", "nav-rocket-guidance", "nav-this-website"];
 
 var currentIndex = 0;
 
 window.addEventListener("scroll", (_) => {
+    changeNavigationBarDisplay(this.scrollY);
+    changeNavButtonColors(this.scrollY);
+})
+
+function changeNavigationBarDisplay(scrollY) {
+    var style = document.getElementById("navbar").style;
+
+    if (scrollY == 0) {
+        style.display = "none";
+    } else {
+        style.display = "flex";
+    }
+}
+
+function changeNavButtonColors(scrollY) {
     // Looks at the vertical scroll position divided by the height of the page to
     // determine which section the user is on. This information is used to determine
     // which of the navigation buttons should be white. (The button that corresponds
     // to the section that the user is at is white, every other button is grey).
 
-    var newIndex = Math.floor((this.scrollY / window.innerHeight) + .1);
+    // The .9 value that appears accounts for the fact that every section is 90% of
+    // the window's total height. 
+
+    var newIndex = (scrollY > .9 * window.innerHeight) 
+        ? Math.floor((scrollY / (.9 * window.innerHeight)) - .9)
+        : -1;
 
     if (newIndex != currentIndex) {
         currentIndex = newIndex;
@@ -19,13 +39,22 @@ window.addEventListener("scroll", (_) => {
                 (i == currentIndex) ? "white" : "rgb(160, 160, 160)";
         }
     }
-})
+}
+
+function goToEntropySection() {
+    // On the intro section, there is a down arrow. When this button is pressed,
+    // the user is taken to the Entropy section (section immediately after intro
+    // section).
+
+    document.getElementById("entropy").scrollIntoView({block: 'center', behavior: 'smooth'});
+}
 
 function changeMenu() {
     // When the user is on a mobile device, the navigation buttons are accessed through
     // a button. This button either opens are closes a menu that contains the navigation
     // buttons. This function is called every time this button is clicked. This function
     // has to determine if the menu is opened or closed, and change things accordingly. 
+
     var style = document.getElementById('nav-buttons').style;
     var image = document.getElementById("menu-button");
 
